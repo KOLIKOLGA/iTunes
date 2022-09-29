7ec5a0c103fb36d37f113389b18f6a88e8a0a706
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-09-29 04:59:20 +0300

    feat: finish videoPlayer

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -10,8 +10,60 @@ export const videoPlayerInit = () => {
   const videoTimePassed = document.querySelector(".video-time__passed");
   const videoTimeTotal = document.querySelector(".video-time__total");
 
-  videoPlayer.addEventListener("click", () => {
-    console.log("click");
-    videoPlayer.play();
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove("fa-pause");
+      videoButtonPlay.classList.add("fa-play");
+    } else {
+      videoButtonPlay.classList.add("fa-pause");
+      videoButtonPlay.classList.remove("fa-play");
+    }
+  };
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+
+    toggleIcon();
+  };
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  };
+  const addZero = (n) => {
+    return n < 10 ? "0" + n : n;
+  }; // добавляет ноль если нет единиц
+  videoPlayer.addEventListener("click", togglePlay);
+  videoButtonPlay.addEventListener("click", togglePlay);
+  videoPlayer.addEventListener("play", toggleIcon);
+  videoPlayer.addEventListener("pause", toggleIcon);
+  videoButtonStop.addEventListener("click", stopPlay);
+
+  videoPlayer.addEventListener("timeupdate", () => {
+    const currentTime = videoPlayer.currentTime; // время пройденной видеозаписи
+    const duration = videoPlayer.duration; // все время записи
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutePassed = Math.floor(currentTime / 60); // сколько минут просмотренно
+    let secondePassed = Math.floor(currentTime % 60); // сколько секурд просмотренно
+
+    let minuteTotal = Math.floor(duration / 60); // сколько минут продолжительность записи
+    let secondeTotal = Math.floor(duration % 60); // сколько секунд к манутам
+
+    videoTimePassed.textContent = `
+    ${addZero(minutePassed)}:${addZero(secondePassed)}
+    `;
+    videoTimeTotal.textContent = `
+    ${addZero(minuteTotal)}:${addZero(secondeTotal)}
+    `;
+  });
+  videoProgress.addEventListener("input", () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+
+    videoPlayer.currentTime = (value * duration) / 100; // можно переместить ползунок видео
   });
 };
