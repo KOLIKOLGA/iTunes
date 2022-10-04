4dc370880de9672a7313bbab1c18793a149f3e4f
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-10-04 21:24:35 +0300

    feat: add radio stantion

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,29 @@
 export const radioPlayerInit = () => {
-  console.log("Radio Init");
+  const radio = document.querySelector(".radio");
+  const radioCoverImg = document.querySelector(".radio-cover__img");
+  const radioHeader = document.querySelector(".radio-header");
+  const radioNavigation = document.querySelector(".radio-navigation");
+  const radioItem = document.querySelectorAll(".radio-item");
+  const radioStop = document.querySelector(".radio-stop");
+
+  const audio = new Audio();
+  audio.type = "audio/aac";
+
+  radioStop.disabled = true;
+
+  radioNavigation.addEventListener("change", (event) => {
+    radioStop.disabled = false;
+    const target = event.target;
+    audio.src = target.dataset.radioStantion;
+
+    audio.play();
+  });
+  radioStop.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    console.log();
+  });
 };
